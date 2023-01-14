a7c67995ae06b86aed4f5d7a0f5fc6adc131d91a
Patrice THIMOTHEE <thimpat@gmail.com>
2023-01-14 15:38:44 +0000

    BUILD: Add template files

---
+++ tpl/test/unit-test.cjs
@@ -0,0 +1,11 @@
+const {expect} = require("chai");
+
+describe("The .cjs module", () =>
+{
+    it("should work", function ()
+    {
+        const result = true;
+        expect(result).to.equal(true);
+    });
+
+});
---
+++ tpl/test/unit-test.spec.ts
@@ -0,0 +1,11 @@
+import {describe, it} from "mocha";
+import {expect} from "chai";
+
+describe("From ts", () =>
+{
+    it("should work", function ()
+    {
+        const result = true;
+        expect(result).to.equal(true);
+    });
+});
